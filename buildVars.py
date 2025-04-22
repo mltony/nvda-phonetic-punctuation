df9ad7dfdc2bb92397b9ca7b6613311f224e758b
mltony <anton.malykh+0@gmail.com>
2025-04-22 10:10:47 -0700

    Update minimum NVDA version

--- buildVars.py
+++ buildVars.py
@@ -30,7 +30,7 @@ Please check out documentation for the full list of available speech rules."""),
     # Documentation file name
     "addon_docFileName": "readme.html",
     # Minimum NVDA version supported (e.g. "2018.3")
-    "addon_minimumNVDAVersion": "2019.3.0",
+    "addon_minimumNVDAVersion": "2025.1",
     # Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
     "addon_lastTestedNVDAVersion": "2025.1",
     # Add-on update channel (default is stable or None)
